57a95a7fdd4c5c2cdfa930cebd0f502013cebaf6
guoyuchuan <1187960220@qq.com>
2018-09-04 21:34:17 +0800

    spring bean的生命周期

---
+++ springboot-bean/src/main/java/com/gyc/GycApplication.java
@@ -0,0 +1,12 @@
+package com.gyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GycApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GycApplication.class, args);
+	}
+}
